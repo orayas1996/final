28157d08f2db0571cbad11f07ce9caabc2c73a29
jane1996 <ritzjane@gmail.com>
2019-02-14 16:08:56 +0700

    Init and teach game

---
+++ app/src/main/java/com/example/acer/mem/teach.java
@@ -0,0 +1,140 @@
+package com.example.acer.mem;
+
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+
+public class teach extends AppCompatActivity {
+
+    private int presCounter = 0;
+    private int maxPresCounter = 2;
+    private String[] keys = {"น", "ฟ", "ก", "ข", "ค","ส","ุ","้" ,"น", "ฟ", "ก", "ข", "ค","ส","ุ","้" };
+    private String textAnswer = "นก";
+    private String textAnswer2 = "สุ";
+    TextView textScreen, textQuestion, textTitle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teach);
+
+
+
+        keys = shuffleArray(keys);
+
+        for (String key : keys) {
+            addView(((LinearLayout) findViewById(R.id.layoutParent)), key, ((EditText) findViewById(R.id.editText)));
+        }
+
+        maxPresCounter = 2;
+    }
+
+
+    private String[] shuffleArray(String[] ar) {
+        Random rnd = new Random();
+        for (int i = ar.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            String a = ar[index];
+            ar[index] = ar[i];
+            ar[i] = a;
+        }
+        return ar;
+    }
+
+
+    private void addView(LinearLayout viewParent, final String text, final EditText editText) {
+        LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        linearLayoutParams.rightMargin = 20;
+
+        final TextView textView = new TextView(this);
+
+        textView.setLayoutParams(linearLayoutParams);
+        textView.setBackground(this.getResources().getDrawable(R.drawable.bgpink));
+        textView.setTextColor(this.getResources().getColor(R.color.colorAccent));
+        textView.setGravity(Gravity.CENTER);
+        textView.setText(text);
+        textView.setClickable(true);
+        textView.setFocusable(true);
+        textView.setTextSize(32);
+
+
+
+
+        textScreen = (TextView) findViewById(R.id.textScreen);
+        textTitle = (TextView) findViewById(R.id.textTitle);
+
+
+
+        textView.setOnClickListener(new View.OnClickListener() {
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                if(presCounter < maxPresCounter) {
+                    if (presCounter == 0)
+                        editText.setText("");
+
+                    editText.setText(editText.getText().toString() + text);
+
+                    textView.animate().alpha(0).setDuration(300);
+                    presCounter++;
+
+                    if (presCounter == maxPresCounter)
+                        doValidate();
+                }
+            }
+        });
+
+
+        viewParent.addView(textView);
+
+
+    }
+
+
+    private void doValidate() {
+        presCounter = 0;
+
+        EditText editText = findViewById(R.id.editText);
+        LinearLayout linearLayout = findViewById(R.id.layoutParent);
+
+        if(editText.getText().toString().equals(textAnswer)) {
+            Toast.makeText(teach.this, "Correct", Toast.LENGTH_SHORT).show();
+        }else if(editText.getText().toString().equals(textAnswer2)){
+            Toast.makeText(teach.this, "Correct", Toast.LENGTH_SHORT).show();
+            editText.setText("");
+        } else {
+            Toast.makeText(teach.this, "Wrong", Toast.LENGTH_SHORT).show();
+            editText.setText("");
+        }
+
+        keys = shuffleArray(keys);
+        linearLayout.removeAllViews();
+        for (String key : keys) {
+            addView(linearLayout, key, editText);
+        }
+
+    }
+
+
+}
